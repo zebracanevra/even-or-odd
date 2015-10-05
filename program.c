cb6b853e8bb9d072738f77bb2881b85683eab9df
Zebra Canevra <zebracanevra@users.noreply.github.com>
2015-10-05 22:56:11 +1100

    add stat even/odd

--- program.c
+++ program.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+
+//todo: better name for this function
+int fileIsEven (char* filename) {
+    //version 1: use 'stat' to find the length.
+    //the length is the amount of characters
+    struct stat sb;
+    if(stat(argv[i], &sb) == 0) {
+        //failed to read file
+        return -1;
+    } else if(!S_ISREG(sb.st_mode)) {
+        //not a file (directory?)
+        return -1;
+    } else {
+        //is a file
+        //if even, modulus 2 will give 0
+        //if odd, will give 1
+        return sb.st_size % 2 == 0;
+    }
+}
 
 int main (int argc, char *argv[])
 {
+    //arguments start at argv[1] to argv[argc-1]
     for (int i = 1; i < argc; i++) {
-        printf("Arg %d: %s\n", i, argv[i]);
+        char* filename = argv[i];
+        printf("Arg %d: %s Even? %d\n", i, filename, fileIsEven(filename));
     }
     return 0;
 }
