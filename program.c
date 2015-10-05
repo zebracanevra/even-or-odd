6509ad9b7eda90627f370592067a10386f2762e9
Zebra Canevra <zebracanevra@users.noreply.github.com>
2015-10-05 23:20:02 +1100

    add forking

--- program.c
+++ program.c
@@ -2,17 +2,21 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+#define EVEN 0
+#define ODD 1
 
 //todo: better name for this function
 int fileIsEven (char* filename) {
     //version 1: use 'stat' to find the length.
     //the length is the amount of characters
     struct stat sb;
-    if(stat(filename, &sb) != 0) {
+    if (stat(filename, &sb) != 0) {
         //failed to read file
         return -1;
-    } else if(!S_ISREG(sb.st_mode)) {
+    } else if (!S_ISREG(sb.st_mode)) {
         //not a file (directory?)
         return -1;
     } else {
@@ -23,17 +27,60 @@ int fileIsEven (char* filename) {
         //yes this can all be done in one line,
         //but making it explicit is better?
         //depends on what skill your markers expect.
-        if(even) return 0;
-        else return 1;
+        if (even) return EVEN;
+        else return ODD;
     }
 }
 
 int main (int argc, char *argv[])
 {
+    int processesForked = 0;
+
     //arguments start at argv[1] to argv[argc-1]
     for (int i = 1; i < argc; i++) {
         char* filename = argv[i];
+        int forkResult = fork();
+        if (forkResult == -1) {
+            //fork failed
+            return -1;
+        } else if (forkResult == 0) {
+            //running in child process:
+            //the return result is the exit code
+            return fileIsEven(filename);
+        } else {
+            //running in parent process:
+            processesForked++;
+        }
         printf("Arg %d: %s Even? %d\n", i, filename, fileIsEven(filename));
     }
-    return 0;
+
+    int currentState = -1;
+
+    for (int i = 0; i < processesForked; i++) {
+        //wait for any of the child process to exit,
+        //copying it's exit code into status
+        int status;
+        if (wait(&status) == -1) {
+            //waiting failed
+            return -1;
+        }
+
+        if (currentState == -1) {
+            //for the first child returned,
+            //simply set the state
+            currentState = status;
+        } else {
+            //0 + 1 % 2 == ODD
+            //1 + 1 % 2 == EVEN
+            currentState = currentState + status % 2;
+        }
+    }
+
+    if (currentState == EVEN) {
+        printf("Even\n");
+    } else if (currentState == ODD) {
+        printf("Odd\n");
+    }
+
+    return currentState;
 }
